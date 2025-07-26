8cc4403ed225b722acc28cc4167ff6b0c4302598
itshoneyhub <khaireps.96@gmail.com>
2025-07-26 15:25:13 +0530

    refactor: Remove unnecessary console.log from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: './.env' });
 
-console.log('Server starting...');
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
